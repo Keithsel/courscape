cdc6cb7cecec7b16409497c3ab6072cdc0f43cf4
Anh Nhữ Quang <anhnqqe180005@fpt.edu.vn>
2025-01-25 16:40:44 +0700

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -4,4 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Default settings
-DEFAULT_BROWSER = os.getenv("DEFAULT_BROWSER", "chrome").lower()  # Default to Chrome
+browser = os.getenv("DEFAULT_BROWSER", "").lower()
+DEFAULT_BROWSER = browser if browser in ["chrome", "chromium", "firefox"] else "chrome"
+
+# Coursera credentials (optional)
+COURSERA_EMAIL = os.getenv("COURSERA_EMAIL")
+COURSERA_PASSWORD = os.getenv("COURSERA_PASSWORD")
