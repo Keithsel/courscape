a8f070cf4d369d9d3c13c9360fecd8fa0c55089e
Anh Nhữ Quang <anhnqqe180005@fpt.edu.vn>
2025-01-27 02:36:08 +0700

    code format

--- download_webdriver.py
+++ download_webdriver.py
@@ -8,30 +8,33 @@ from loguru import logger
 from config import DEFAULT_BROWSER
 import logging
 
+
 class WebDriverManager:
     def __init__(self, browser=DEFAULT_BROWSER, headless=True):
         self.browser = browser
         self.headless = headless
-        logging.getLogger('WDM').disabled = True
-        logger.debug(f"Initializing WebDriverManager with browser={browser}, headless={headless}")
+        logging.getLogger("WDM").disabled = True
+        logger.debug(
+            f"Initializing WebDriverManager with browser={browser}, headless={headless}"
+        )
 
     def get_driver(self):
         """
         Initialize a WebDriver instance for Chrome/Chromium or Firefox.
-        
+
         Returns:
             WebDriver: Configured WebDriver instance.
-        
+
         Raises:
             ValueError: If the browser type is not supported.
         """
         logger.debug(f"Getting driver for {self.browser}")
         try:
-            if self.browser == 'chrome':
+            if self.browser == "chrome":
                 return self._get_chrome_driver()
-            elif self.browser == 'chromium':
+            elif self.browser == "chromium":
                 return self._get_chromium_driver()
-            elif self.browser == 'firefox':
+            elif self.browser == "firefox":
                 return self._get_firefox_driver()
             else:
                 raise ValueError(f"Unsupported browser: {self.browser}")
@@ -45,7 +48,7 @@ class WebDriverManager:
             options = webdriver.ChromeOptions()
             if self.headless:
                 logger.debug("Enabling headless mode for Chrome")
-                options.add_argument('--headless')
+                options.add_argument("--headless")
 
             service = ChromeService(ChromeDriverManager().install())
             driver = webdriver.Chrome(service=service, options=options)
@@ -60,8 +63,8 @@ class WebDriverManager:
         try:
             options = webdriver.ChromeOptions()
             if self.headless:
-                options.add_argument('--headless')
-            
+                options.add_argument("--headless")
+
             service = ChromeService(
                 ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
             )
@@ -77,8 +80,8 @@ class WebDriverManager:
         try:
             options = webdriver.FirefoxOptions()
             if self.headless:
-                options.add_argument('--headless')
-            
+                options.add_argument("--headless")
+
             service = FirefoxService(GeckoDriverManager().install())
             driver = webdriver.Firefox(service=service, options=options)
             logger.success("Firefox driver setup successful")
